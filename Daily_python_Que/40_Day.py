44d5dd31506d35ed8a364016ecad8f0023c633f8
kshitij katore <kshitijkatore03@gmail.com>
2024-02-28 20:35:53 +0530

    update folder

---
+++ Daily_python_Que/40_Day.py
@@ -0,0 +1,11 @@
+x = {}
+y = [1, 2]
+z = [3, 4]
+
+print(x.fromkeys(y, z))
+
+# What is the output of the following code?
+# A. {1: [3, 4], 2: [3, 4]}
+# B. {1: 3, 2: 4}
+# C. {1: [3, 4], 2: [3, 4]}
+# D. {1: 3, 2: 4}
