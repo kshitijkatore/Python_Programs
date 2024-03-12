74c5789cc5639ec4c29bbd45f561e24e06dec849
kshitij katore <kshitijkatore03@gmail.com>
2024-03-12 23:02:22 +0530

    Day_51

---
+++ Daily_python_Que/51_Day.py
@@ -0,0 +1,12 @@
+g = (1, 2, 3)
+h = (1, 2, 3)
+
+print(f"g is h: {g is h}")
+print(f"g == h: {g == h}")
+
+# What is the output of the following code?
+# a) True False
+# b) False True
+# c) True True
+# d) False False
+
