13caf251321df98b0825e38cb6c155e016fc8fb6
kshitij katore <kshitijkatore03@gmail.com>
2024-03-16 23:26:23 +0530

    Day_53

---
+++ Daily_python_Que/53_day.py
@@ -0,0 +1,12 @@
+def func():
+    try:
+        return 10
+    finally:
+        return 20
+print(func())
+
+# What is the output of the folling code snippet?
+# a) 10
+# b) 20
+# c) None
+# d) 10 20
