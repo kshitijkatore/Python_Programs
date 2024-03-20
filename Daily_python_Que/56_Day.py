f3115a72da265b4da0c91e2ef8217530a5ed883f
kshitij katore <kshitijkatore03@gmail.com>
2024-03-20 21:37:23 +0530

    Day_56

---
+++ Daily_python_Que/56_Day.py
@@ -0,0 +1,17 @@
+user = {
+    "name": "xitij",
+    "age": 30,
+}
+
+name_check = "xitij"
+
+if name_check in user:
+    print(f'found {name_check}')
+else:
+    print(f'Did not found {name_check}')
+
+# What is the output of this code?
+# a)Did nt find alice
+# b)found alice
+# c)Error
+# d)None of the above
